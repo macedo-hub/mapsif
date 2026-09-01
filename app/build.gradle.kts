d194868223e2493006795ccc4b46b57bf22cdc4e
Lucas Macêdo <lucasemanuelnobrega@gmail.com>
2026-09-01 19:00:50 -0300

    Projeto limpo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "br.com.example.mapsif"
-        minSdk = 31
+        minSdk = 21
         targetSdk = 37
         versionCode = 1
         versionName = "1.0"
